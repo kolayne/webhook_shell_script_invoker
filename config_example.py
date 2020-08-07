6e3d45591ee09332212a36de97236d0b62c5c2af
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2020-08-07 18:34:14 +0300

    Update config_example.py
    Codacy thinks `'123456'` is a security issue. Let's make it happy

--- config_example.py
+++ config_example.py
@@ -1,4 +1,4 @@
-secret_token = '123456'
+secret_token = 'ujeiuqeojqnbijnzlkjcoqituqioenuiniuf'  # Some secret string
 projects_to_scripts = {'project_name_1': 'script_name_1.sh'}
 # PLEASE, NOTE, that values of `projects_to_scripts` are just `path.join`ed with `'scripts/'` and the resulting string
 # is ran. Be careful, with giving access to this file
